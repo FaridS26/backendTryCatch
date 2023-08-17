69fec6370d2a130232210786e86bd5e898c2ee87
AndresRomeroR <46949598+AndresRomeroR@users.noreply.github.com>
2023-08-17 11:41:11 -0500

    Add files via upload
    use rcontroller

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -5,31 +5,91 @@ namespace test_tryCatch.Controller;
 
 [ApiController]
 [Route("/tickets")]
-public class TicketsController: ControllerBase{
-  private readonly DataContext _context;
-  public TicketsController(DataContext context)
-  {
-    _context = context;
-  }
+public class TicketsController : ControllerBase
+{
+    private readonly DataContext _context;
+    public TicketsController(DataContext context)
+    {
+        _context = context;
+    }
 
- [HttpGet("id_client/{id_client}")]
-  public IEnumerable<Ticket> GetTicketsByClient(int id_client){
-    return _context.Tickets.Where(b => b.IdUser == id_client).ToList();
-  }
-  [HttpGet("id_ticket/{id_ticket:int}")]
-  public ActionResult<Ticket> GetTicketsById(int id_ticket){
-     var ticketById = _context.Tickets.Find(id_ticket);
-     if(ticketById is null)
-      return NotFound();
-    return ticketById;
-  }
-  // Post Method
-  [HttpPost]
-  public IActionResult CreateTicket(Ticket ticket){
-    _context.Tickets.Add(ticket);
-    _context.SaveChanges();
+    [HttpGet("id_client/{id_client}")]
+    public IEnumerable<Ticket> GetTicketsByClient(int id_client)
+    {
+        return _context.Tickets.Where(b => b.IdUser == id_client).ToList();
+    }
+    [HttpGet("id_ticket/{id_ticket:int}")]
+    public ActionResult<Ticket> GetTicketsById(int id_ticket)
+    {
+        var ticketById = _context.Tickets.Find(id_ticket);
+        if (ticketById is null)
+            return NotFound();
+        return ticketById;
+    }
+    // Post Method
+    [HttpPost]
+    public IActionResult CreateTicket(Ticket ticket)
+    {
+        _context.Tickets.Add(ticket);
+        _context.SaveChanges();
 
-    return CreatedAtAction(nameof(GetTicketsById), new {id_ticket = ticket.Id}, ticket);
-  }
-  
+        return CreatedAtAction(nameof(GetTicketsById), new { id_ticket = ticket.Id }, ticket);
+    }
+
+    // Delete ticket
+    [HttpDelete("ticket/{id:int}")]
+    public ActionResult<Ticket> DeleteTicket(int id)
+    {
+        var ticketChange = _context.Tickets.Find(id);
+        if (User is null)
+            return NotFound();
+
+        ticketChange.Status = "inactve";
+        _context.SaveChanges();
+
+        return Ok();
+    }
+
+    // Put Method
+    [HttpPut("ticket/{id:int}")]
+    public IActionResult UpdateTicket(int id, Ticket ticket)
+    {
+        if (id != ticket.Id)
+            return BadRequest();
+
+        var existingTicket = _context.Tickets.Find(id);
+        if (existingTicket is null)
+            return NotFound();
+
+
+        existingTicket.IdUser = ticket.IdUser;
+        existingTicket.Direccion = ticket.Direccion;
+        existingTicket.Lat = ticket.Lat;
+        existingTicket.Lng = ticket.Lng;
+        existingTicket.Descripcion = ticket.Descripcion;
+        existingTicket.ServiceType = ticket.ServiceType;
+        existingTicket.UpdatedAt = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Local);
+        existingTicket.Status = ticket.Status;
+        existingTicket.Priority = ticket.Priority;
+        existingTicket.ServiceDate = ticket.ServiceDate;
+
+
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    // Close Method
+    [HttpDelete("ticketclose/{id:int}")]
+    public ActionResult<Ticket> CloseTicket(int id)
+    {
+        var ticketChange = _context.Tickets.Find(id);
+        if (User is null)
+            return NotFound();
+
+        ticketChange.Status = "close";
+        _context.SaveChanges();
+
+        return Ok();
+    }
 }
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using test_tryCatch.Data.Models;
+using test_tryCatch.Data;
+
+namespace test_tryCatch.Controllers
+{
+    [ApiController]
+    [Route("/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet] // Get all Users
+        public IEnumerable<Usuario> GetEmployees()
+        {
+            return _context.Usuarios.Where(u => u.Status == "active").ToList();
+        }
+
+        [HttpGet("/user/{cedula}")] // Get user by personal ID
+        public ActionResult<Usuario> GetUserByIdCard(string cedula)
+        {
+            var user = _context.Usuarios.FirstOrDefault(u => u.Cedula == cedula);
+            if (user == null)
+                return NotFound();
+            return user;
+        }
+
+        [HttpGet("user/{id:int}")] // Get user by id in database
+        public ActionResult<Usuario> GetUserById(int id)
+        {
+            var user = _context.Usuarios.Find(id);
+            if (user is null)
+                return NotFound();
+            return user;
+        }
+
+        // Create user
+        [HttpPost]
+        public IActionResult CreateUser(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetUserById), new { id = usuario.Id }, usuario);
+        }
+
+        // Put Method
+        [HttpPut("user/{id:int}")]
+        public IActionResult UpdateUser(int id, Usuario usuario)
+        {
+            if (id != usuario.Id)
+                return BadRequest();
+
+            var existingUser = _context.Usuarios.Find(id);
+            if (existingUser is null)
+                return NotFound();
+
+
+            existingUser.Nombres = usuario.Nombres;
+            existingUser.Apellidos = usuario.Apellidos;
+            existingUser.Celular = usuario.Celular;
+            existingUser.Email = usuario.Email;
+            existingUser.Direccion = usuario.Direccion;
+            existingUser.UpdatedAt = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Local);
+            existingUser.Status = usuario.Status;
+            existingUser.FechaNacimiento = usuario.FechaNacimiento;
+            existingUser.Lat = usuario.Lat;
+            existingUser.Lng = usuario.Lng;
+
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // Delete user
+        [HttpDelete("user/{id:int}")]
+        public ActionResult<Usuario> DeleteUser(int id)
+        {
+            var User = _context.Usuarios.Find(id);
+            if (User is null)
+                return NotFound();
+
+            User.Status = "inactive";
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
